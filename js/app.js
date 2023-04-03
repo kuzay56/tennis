0067e25958b916465306915c6425da9526d0da26
Igor <igorkuznetsov2011@mail.ru>
2023-04-03 16:48:19 +0500

    slider-ready

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 6,
+	spaceBetween: 54,
+	freeMode: true,
+	navigation: {
+		nextEl: ".arrow__next",
+		prevEl: ".arrow__previous",
+	},
+	keyboard: true,
+});
