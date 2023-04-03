9c860606b2aabf0f046abd123ac94059a8346597
Igor <igorkuznetsov2011@mail.ru>
2023-04-03 21:47:28 +0500

    ready

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper1 = new Swiper(".swiper1", {
 	slidesPerView: 6,
-	spaceBetween: 54,
+	spaceBetween: 50,
 	freeMode: true,
 	navigation: {
 		nextEl: ".arrow__next",
 		prevEl: ".arrow__previous",
 	},
 	keyboard: true,
+},);
+
+var swiper2 = new Swiper(".swiper2", {
+	spaceBetween: 100,
+	navigation: {
+		nextEl: ".arrow__next-2",
+		prevEl: ".arrow__previous-2",
+	},
 });
